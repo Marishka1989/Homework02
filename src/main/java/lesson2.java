9dfb03ae81f17557c8985ab760d85a7708bfa665
Марина Горбачева <mariwka-2211mail.ru>
2021-04-05 22:37:27 +0700

    Initial commit

---
+++ src/main/java/lesson2.java
@@ -0,0 +1,31 @@
+public class lesson2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean within10and20(int x1, int x2) {
+        int sum = x1 + x2;
+        return sum > 10 && sum < 20;
+        }
+
+    public static void isPositiveOrNegative(int x) {
+        if (x >=0) {
+            System.out.println("положительное число");
+        } else {
+            System.out.println("отрицательное число");
+        }
+    }
+    public static boolean isNegative(int x) {
+        return x >= 0;
+    }
+    public static void printWordNTimes(String word, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println("String");
+        }
+        }
+
+    }
+
+
+
